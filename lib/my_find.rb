81e533e3a78a86937b66704673e383cfd6441c6e
Kriti Rai <kriti.rai@gmail.com>
2018-01-10 05:42:32 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,10 +4,10 @@ def my_find(collection)
   i = 0
   while i < collection.length
     yield(collection[i])
+    binding.pry
     i+=1
   end
 end
 
 collection = (1..100).to_a
 my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
-binding.pry
